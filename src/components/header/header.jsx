ce08bb065152d070bd5a81fa459e8a1ec56beaa5
Liubou Tserakhava <Liubou.Tserakhava@itechart-group.com>
2020-02-04 16:08:13 +0300

    FIXED SPACES

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -5,7 +5,7 @@ import IconPlane from '../png/plane.png';
 import { Menu } from './menu';
 
 export const Header = () => (
-  <div className="header ">
+  <div className="header">
     <div className="plane-image"><img src={IconPlane} alt="plane" /></div>
     <div>
       <img src={Iconlogo} alt="Logo" />
